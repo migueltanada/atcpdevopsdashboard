db128fe09537e21224abcf7a551b92f0c8d01e32
Renn Pineda <renn.louie.s.pineda@accenture.com>
2019-02-22 03:47:58 +0800

    Changed project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular7-bootstrap4-demo!');
+    expect(page.getTitleText()).toEqual('Welcome to atcpdevopsdashboard!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular7-bootstrap4-demo'`, () => {
+  it(`should have as title 'atcpdevopsdashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular7-bootstrap4-demo');
+    expect(app.title).toEqual('atcpdevopsdashboard');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular7-bootstrap4-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to atcpdevopsdashboard!');
   });
 });
