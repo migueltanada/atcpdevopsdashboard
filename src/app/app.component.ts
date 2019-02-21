0ff7e793b23517292c2c3f6e5254ef7dcabd716b
Renn Pineda <renn.louie.s.pineda@accenture.com>
2019-02-22 02:55:23 +0800

    Angular scope for Edit-Save button and Tools sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevOps Dashboard';
+  navbarDescription = 'DevOps Platform'
   public shouldShow = true;
+  public editButton: string = 'Edit';
+  public changeText(): void {
+    if(this.editButton === 'Edit') { 
+      this.editButton = 'Save'
+    } else {
+      this.editButton = 'Edit'
+    }
+  }
 }
